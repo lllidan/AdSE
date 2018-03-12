cc4c79c04fd2653bf60ead702a6c93d07aea5a72
Zexin Young <lllidanismine@gmail.com>
2018-03-12 22:35:38 +0800

    单人项目代码完成（1）

--- sizeyunsuan/src/main1.py
+++ sizeyunsuan/src/main1.py
@@ -5,6 +5,14 @@ from fractions import Fraction
 
 
 def threeParameter(total, scope,jingdu,negative):
+    """
+
+    :param total: 生成计算题的数量
+    :param scope:题中每个因子的范围
+    :param jingdu:小数的精度
+    :param negative:是否包含有负数
+    function: 根据传入的参数，定制打印出的题目数
+    """
     oscope = -scope
     num1 = 0
     num2 = 0
@@ -15,6 +23,8 @@ def threeParameter(total, scope,jingdu,negative):
     eval_combine = []
     eval_combine_str = ""
     questions = set()
+    ans = 0
+    anses = ""
     while True:
         if negative == 'y':
             num1 = random.randint(oscope, scope)
@@ -37,9 +47,12 @@ def threeParameter(total, scope,jingdu,negative):
             questions.add(eval_combine_str)
         else:
             continue
-        sums = str(round(eval(eval_combine_str),jingdu))
+        sums = str(round(eval(eval_combine_str), jingdu))
 
-        print(eval_combine_str)
+        anses = anses + str(counts) + "、" + str(sums) + "  "
+
+
+        print(eval_combine_str + "=")
         ans = input()
         if ans == sums:
             print("你对了!")
@@ -52,7 +65,7 @@ def threeParameter(total, scope,jingdu,negative):
             calscore(true, number)
             writeYN = input("是否要将本次的练习题写入到文件中？(y/n)")
             if writeYN == 'y':
-                writetofile(questions)
+                writetofile(questions, anses)
             break
         else:
             counts += 1
@@ -61,7 +74,21 @@ def threeParameter(total, scope,jingdu,negative):
 def twoParameter(number):
     """
     最简单的计算函数，接收一个题目数量的参数number，然后打印简单的题
-    :param number: 题目数量
+    :param
+        number: 题目数量
+    :var
+        num1:第一个操作数
+        num2:第二个操作数
+        counts:计算的次数
+        score:分数
+        true:做对的题的数目
+        eval_combine:组合的表达式
+        eval_combine_str:存储组合形成的表达式的字符串
+        questions:存储全部的题目
+        sums:存储标准答案
+        ans:存储用户输入的答案
+        anses：存储全部的标准答案
+
     :return:
     """
     num1 = 0
@@ -72,6 +99,9 @@ def twoParameter(number):
     eval_combine = []
     eval_combine_str = ""
     questions = set()
+    ans = 0
+    anses = ""
+
     print("除法结果保留两位小数，整数保留小数点后第一个零")
     while True:
         num1 = random.randint(-10, 10)
@@ -87,6 +117,9 @@ def twoParameter(number):
         eval_combine_str = "".join(eval_combine)
 
         sums = str(round(eval(eval_combine_str), 2))
+
+        anses = anses + str(counts) + "、" + str(sums) + "  "
+
         if eval_combine_str not in questions:
             questions.add(eval_combine_str)
         else:
@@ -107,34 +140,131 @@ def twoParameter(number):
             calscore(true,number)
             writeYN = input("是否要将本次的练习题写入到文件中？(y/n)")
             if writeYN == 'y':
-                writetofile(questions)
+                writetofile(questions,anses)
             break
         else:
             counts += 1
 
-def calscore(true,number):
-    score = 100 * (true / number)
+
+def testpaper(number, scope, jingdu, negative):
+    oscope = -scope
+    num1 = 0
+    num2 = 0
+    num3 = 0
+    counts = 1
+    score = 0
+    true = 0
+    eval_combine = []
+    eval_combine_str = ""
+    questions = set()
+    ans = 0
+    anses = ""
+    while True:
+        print("第%d道" % counts)
+        if negative == 'y':
+            num1 = random.randint(oscope, scope)
+            num2 = random.randint(oscope, scope)
+            num3 = random.randint(oscope, scope)
+        else:
+            num1 = random.randint(1, scope)
+            num2 = random.randint(1, scope)
+            num3 = random.randint(1, scope)
+        s1 = random.choice("+-*/")
+        s2 = random.choice("+-*/")
+        eval_combine.append(str(num1))
+        eval_combine.append(str(s1))
+        eval_combine.append(str(num2))
+        eval_combine.append(str(s2))
+        eval_combine.append(str(num3))
+
+        eval_combine_str = "".join(eval_combine)
+        if eval_combine_str not in questions:
+            questions.add(eval_combine_str)
+        else:
+            continue
+
+        try:
+            sums = str(round(eval(eval_combine_str), jingdu))
+        except ZeroDivisionError as e:
+            print("except:", e)
+            eval_combine = []
+            eval_combine_str = ""
+            continue
+
+
+        anses = anses + str(counts) + "、" + str(sums) + "  "
+        eval_combine = []
+        eval_combine_str = ""
+
+        if counts is number:
+            break
+        else:
+            counts = counts + 1
+
+
+    name = "testpapaer.txt"
+    with open(name, 'w') as f:
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime(time.time())))
+        f.write("欢迎参加考试，考试题的规则如下：\n")
+        f.write("考试题目共有%d道\n" % number)
+        f.write("考试题目中出现的最大数字为%d\n" % scope)
+        f.write("试题答案应该保留的精度为%d位\n" % jingdu)
+        if negative == 1:
+            f.write("题目中包含负数\n")
+        else:
+            f.write("题目中不不包含负数\n")
+
+        f.write("----------考试题目如下---------")
+        for question in questions:
+            f.write(question + "=\n")
+        f.write("----------所有题的答案如下：------------\n")
+        f.write(anses)
+    print("题目文件生成成功，名字为%s" % name)
+
+
+def calscore(trues,numbers):
+    score = 100 * (trues / numbers)
     print("您这次的总分是%d" % (score))
 
 
-def writetofile(strs):
+def writetofile(questions, anses):
     name = time.strftime('%Y-%m-%d-%H_%M_%S', time.localtime(time.time()))
     name = name+".txt"
     with open(name, 'w') as f:
-        f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + "\n")
-        f.write("题目为：" + "\n")
-        for str in strs:
-            f.write(str+"=\n")
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime(time.time())))
+        f.write("题目如下：\n")
+        for question in questions:
+            f.write(question+"=\n")
+        f.write("所有题的答案如下：\n")
+        f.write(anses)
     print("题目文件生成成功，名字为%s"%name)
 
 
-number = 0
-number = int(input("请输入题目的数量："))
+
+
+
+
+print("欢迎使用数学计算题学习系统：")
 difficult = 0
-difficult = int(input("请输入选择的类型：1、简单(两个操作数的小数字运算)2、多操作数的大数字计算"))
-if difficult == 1:
-    twoParameter(number)
-else:
+number = 0
+mode = 0
+mode = int(input("请输入需要选择的模式：1、练习模式 2、考试模式"))
+if mode == 1:
+    difficult = int(input("请输入选择的类型：1、简单(两个操作数的小数字运算)2、多操作数的大数字计算"))
+    number = int(input("请输入题目的数量："))
+    if difficult == 1:
+        twoParameter(number)
+    else:
+        scope = 0
+        scope = int(input("请输入数字的范围："))
+        jingdu = 0
+        jingdu = int(input("请输入保存的精度："))
+        negative = ''
+        negative = input("操作数中是否包含有负数(y/n)")
+        print("开始答题，结果中是循环小数的话请保留所指定的精度")
+        threeParameter(number,scope,jingdu,negative) # 执行函数Subject_Creater()
+else :
+    number = int(input("请输入题目的数量："))
     scope = 0
     scope = int(input("请输入数字的范围："))
     jingdu = 0
@@ -142,4 +272,4 @@ else:
     negative = ''
     negative = input("操作数中是否包含有负数(y/n)")
     print("开始答题，结果中是循环小数的话请保留所指定的精度")
-    threeParameter(number,scope,jingdu,negative) # 执行函数Subject_Creater()
+    testpaper(number, scope, jingdu, negative)
